10642d829b8a9f885c15764ba435e34779d06435
benjamin austin <benjamin.austin@homeadvisor.com>
2020-09-26 12:05:48 -0600

    [master] Comment out output object in webpack.dev.config.js and add HotModuleReplacement configs therein too. Also: add scoped style tag to App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,12 @@
   <div>
     <h1>Hola Mundo! from webpack-from-scratch-2</h1>
     <p>Can you see me yet?</p>
-    <span style="color: red;">How bout now?</span>
+    <span style="color: yellow; background-color: brown">How bout now?</span>
   </div>
 </template>
+
+<style scoped>
+h1 {
+  border: 1px solid black;
+}
+</style>
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,13 +3,22 @@
 const path = require('path');
 const { VueLoaderPlugin } = require('vue-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
   entry: ['./src/main.js'],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist'),
+  // with the inject: true property from html-webpack-plugin (below), this output stuff stopped working
+  // So, I'ma comment it out for now
+  // output: {
+  //   filename: 'bundle.js',
+  //   path: path.resolve(__dirname, './dist'),
+  // },
+  devServer: {
+    hot: true,
+    watchOptions: {
+      poll: true,
+    },
   },
   module: {
     rules: [
@@ -26,5 +35,6 @@ module.exports = {
       template: 'index.html',
       inject: true,
     }),
+    new webpack.HotModuleReplacementPlugin(),
   ],
 };
