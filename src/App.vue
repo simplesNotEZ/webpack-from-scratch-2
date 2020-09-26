2f575c8cef8d5f82a2b87f525c6f22d629c6f85d
benjamin austin <benjamin.austin@homeadvisor.com>
2020-09-26 13:31:31 -0600

    [master] Add rule to webpack.dev.config.js for .css files. Add loaders for these: vue-style-loader and css-loader. Still not applying css, though no errors

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <h1>Hola Mundo! from webpack-from-scratch-2</h1>
+    <h1 class="h1-class">Hola Mundo! from webpack-from-scratch-2</h1>
     <p>Can you see me yet?</p>
-    <span style="color: yellow; background-color: brown">How bout now?</span>
+    <span style="color: yellow; background-color: red">How bout now?</span>
   </div>
 </template>
 
 <style scoped>
 h1 {
+  background-color: yellow;
   border: 1px solid black;
+  color: green;
 }
 </style>
