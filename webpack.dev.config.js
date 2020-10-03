e1fe082fe2e2f2f6d0ccb658d874dd1fd95202e1
benjamin austin <benjamin.austin@homeadvisor.com>
2020-10-03 13:42:43 -0600

    [master] Install file-loader...again

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -5,18 +5,19 @@ const { VueLoaderPlugin } = require('vue-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: ['./src/main.js'],
   // with the inject: true property from html-webpack-plugin (below), this output stuff stopped working
   // So, I'ma comment it out for now
-  // output: {
-  //   filename: 'bundle.js',
-  //   path: path.resolve(__dirname, './dist'),
-  //   publicPath: 'dist/',
-  // },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    // publicPath: 'dist/',
+    publicPath: 'dist/',
+  },
   devServer: {
     hot: true,
     watchOptions: {
@@ -30,10 +31,10 @@ module.exports = {
         test: /\.vue$/,
         use: 'vue-loader',
       },
-      // {
-      //   test: /\.(png|jpg)$/,
-      //   use: ['file-loader'],
-      // },
+      {
+        test: /\.(png|jpg)$/,
+        use: ['file-loader'],
+      },
       {
         test: /\.css$/,
         use: ['vue-style-loader', MiniCssExtractPlugin.loader, 'css-loader'],
@@ -69,14 +70,14 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'main.css',
     }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: './src/images',
-          to: './dist/images',
-        },
-      ],
-    }),
+    // new CopyWebpackPlugin({
+    //   patterns: [
+    //     {
+    //       from: './src/images',
+    //       to: './dist/images',
+    //     },
+    //   ],
+    // }),
   ],
   resolve: {
     extensions: ['*', '.js', '.vue', '.json'],
